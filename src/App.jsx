9df0b590f5704d4a8be37f144a46ef9c90d519b3
Nerf-Ivan <ivan.swanepoel.dev@gmail.com>
2025-07-19 13:42:28 +0200

    Fixed initial homepage load issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-
-import Home from "./pages/Home"
 import './css/App.css'
 import { Routes, Route } from "react-router-dom"
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
 import { MovieProvider } from "./contexts/MovieContext";
+import Home from "./pages/Home"
 
 function App() {
-
-
   return (
     <MovieProvider>
-      <NavBar/>
+      <NavBar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,9 +16,7 @@ function App() {
         </Routes>
       </main>
     </MovieProvider>
-
   );
 }
 
-
-export default App
+export default App;
